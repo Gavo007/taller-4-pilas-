9e4cd64e432293b9c78e34f04a500905d4cd8260
Gavo007 <gaviriamaciasn@gmail.com>
2026-04-05 23:10:31 -0500

    Update Form1.cs

--- ExpressionEvaluator.UI.Win/Form1.cs
+++ ExpressionEvaluator.UI.Win/Form1.cs
@@ -1,10 +1,46 @@
+using System;
+using System;
+using System.Windows.Forms;
+
 namespace ExpressionEvaluator.UI.Win
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() { InitializeComponent(); }
+
+        private void btnGenerico_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+            if (sender is Button boton)
+            {
+                if (lblPantalla.Text == "Error") lblPantalla.Text = "";
+                lblPantalla.Text += boton.Text;
+            }
+        }
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string expresion = lblPantalla.Text;
+                double resultado = ExpressionEvaluator.Core.Evaluator.Evaluate(expresion);
+                lblPantalla.Text = resultado.ToString();
+            }
+            catch { lblPantalla.Text = "Error"; }
+        }
+
+   
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblPantalla.Text = "";
+        }
+
+        
+        private void btnDetale_Click(object sender, EventArgs e)
+        {
+            if (lblPantalla.Text.Length > 0 && lblPantalla.Text != "Error")
+            {
+               
+                lblPantalla.Text = lblPantalla.Text.Substring(0, lblPantalla.Text.Length - 1);
+            }
         }
     }
 }
